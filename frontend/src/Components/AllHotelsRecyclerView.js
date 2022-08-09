74cd0c77a0a36a2d37a19f08092ac0bfd4a871f3
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-09 19:41:14 +0530

    Dashboard Created

---
+++ frontend/src/Components/AllHotelsRecyclerView.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import '../Styles/AllHotelsRecyclerView.css';
+function AllHotelsRecyclerView() {
+  return (
+    <div className = "wrapperDiv">
+     <Card
+    title="Hotel Aira Xing by Staybook ( 23380 )"
+    description="1/5, Desh Bandhu Gupta Rd, opposite Police Station, Motia Khan, Paharganj, New Delhi, Delhi 110055"
+  />
+
+<Card
+    title="Staybook Jyoti Mahal A Heritage Hotel ( 23539 )"
+    description="2488"
+  />
+
+<Card
+    title="Staybook Hotel Pinky Villa ( 23540 )"
+    description="XV-2463-N, Nalwa St, Chuna Mandi, Paharganj, New Delhi, Delhi 110055"
+  />
+  <Card
+    title="hotel jai bala ji ( 23690 )"
+    description="9918, Plot No. 9918, Street, Multani Dhanda, Paharganj, Delhi, 110055"
+  />
+  <Card
+    title="Staybook South Delhi ( 23719 )"
+    description="B-14 East Of Kailash New Delhi"
+  />
+  <Card
+    title="Staybook Woods View ( 23827 )"
+    description="Camels Back Road Near Ambedkar Chowk Library"
+  />
+  <Card
+    title="Staybook Hotel Geeson ( 23998 )"
+    description="65, Desh Bandhu Gupta Rd, Arya Nagar, Paharganj, Delhi, 110055"
+  />
+  
+  </div>
+
+  
+    
+  )
+}
+
+function Card(props) {
+    return (
+      <div className="card">
+        <div className="card__body">
+          
+          <h2 className="title">{props.title}</h2>
+          <p className="description">{props.description}</p>
+          {/* <h4 className="goToDashBoard">{props.dashboard}</h4> */}
+        </div>
+        <button className="btn-3">Go To Dashboard</button>
+      </div>
+    );
+  }
+
+export default AllHotelsRecyclerView
---
+++ frontend/src/Pages/AllHotelsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AllHotelsRecyclerView from '../Components/AllHotelsRecyclerView.js';
+import '../Styles/AllHotelsPage.css';
+function AllHotelsPage() {
+  return (
+   <> <div className = "dashBoardName">Welcome to the Dashboard</div>
+   <AllHotelsRecyclerView/>
+   </>
+  
+  )
+}
+
+export default AllHotelsPage
