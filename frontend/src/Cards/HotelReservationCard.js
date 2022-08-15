031fb349a063aba90a6d09e13522470c4e367bb3
Anjanay Raina <anjanayraina326@gmail.com>
2022-08-15 12:54:40 +0530

    Some UI Changes in cards

--- frontend/src/Cards/HotelReservationCard.js
+++ frontend/src/Cards/HotelReservationCard.js
@@ -6,12 +6,13 @@ function HotelReservationCard(props) {
     <div className="card__body">
       <div className = "NameWrapper">
       <h2 className="title">{props.name}</h2>
-      <h5 className = "title">{props.bookingID}</h5>
-      </div>
       <p className="description">{props.phoneNumber}</p>
+      <h5 className = "bookingID">{props.bookingID}</h5>
+      </div>
+      
       {/* <h4 className="goToDashBoard">{props.dashboard}</h4> */}
     </div>
-    <button className="btn-3">Go To Dashboard</button>
+    
   </div>
   );
 }
