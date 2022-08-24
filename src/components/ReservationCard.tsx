b84b3d3748da7e54f5ea5ab6b006a34ca99325b9
VisShon <vishnu20414@iiitd.ac.in>
2022-08-25 03:37:23 +0530

    updates

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../styles/ReservationCard.css'
-function HotelReservationCard({name, email ,checkIn, checkOut, amountPaid}:any) {
+function HotelReservationCard({plans,name, email ,checkIn, checkOut, amountPaid}:any) {
   return (
-    <div className="reservationCard">
+    <>
+      <div className="reservationCard">
       
       <div>
           <h2 className="title">{name}</h2>
@@ -10,12 +11,19 @@ function HotelReservationCard({name, email ,checkIn, checkOut, amountPaid}:any) 
         </div>
 
         <>
-          <h2 className="title"> Check In {checkIn}</h2>
-          <h2 className="title">Check Out {checkOut}</h2>
+          <h2 className="title"> Check In {checkIn.substring(0, 10)}</h2>
+          <h2 className="title">Check Out {checkOut.substring(0, 10)}</h2>
         </>
 
         <h5  style ={{color: '#CF8F24', fontSize:'1.5rem'}}>&#8377;{amountPaid}</h5>
-  </div>
+     </div>
+     <div className="reservationCardRoomContainer">
+      {Object.values(plans).map((item:any, index:number) =>(<div className="reservationCardRoom" key={index}>
+          <h2 className="title">{item.roomType}</h2>
+          <p className="description">{item.title}</p>
+        </div>))}
+     </div>
+    </>
   );
 }
 
