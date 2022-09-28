7c356d14b39a42ac8aaf0d29a8e82e452e8ccb78
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2022-09-28 21:01:25 +0530

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,15 @@ import {getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBg3QZs7XNtzzp8nfzYOaVr2tpgXRhSMwM",
-    authDomain: "staybook-7d6bb.firebaseapp.com",
-    databaseURL: "https://staybook-7d6bb-default-rtdb.firebaseio.com",
-    projectId: "staybook-7d6bb",
-    storageBucket: "staybook-7d6bb.appspot.com",
-    messagingSenderId: "620990788027",
-    appId: "1:620990788027:web:ae862bfd5fb02aebc9549e",
-    measurementId: "G-EBW2DWVQNT"
-  };
+  apiKey: "AIzaSyD3MjpR_Ta6LRfoMOaoX-b-kTlVHuiuHtw",
+  authDomain: "staybook-de712.firebaseapp.com",
+  databaseURL: "https://staybook-de712-default-rtdb.firebaseio.com",
+  projectId: "staybook-de712",
+  storageBucket: "staybook-de712.appspot.com",
+  messagingSenderId: "946000733513",
+  appId: "1:946000733513:web:662ce04b970b3d8eb66698",
+  measurementId: "G-V29JV44EFX"
+};
   
   const app = initializeApp(firebaseConfig);
   const database = getDatabase(app);
